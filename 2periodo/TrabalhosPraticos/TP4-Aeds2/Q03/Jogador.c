6101fb356cf13ea4709171a143bba8feb99e7aa3
Yago Almeida Melo <yagoalmeidamelo27102004@gmail.com>
2023-12-07 21:14:30 -0300

    Q07 100%

--- 2periodo/TrabalhosPraticos/TP4-Aeds2/Q03/Jogador.c
+++ 2periodo/TrabalhosPraticos/TP4-Aeds2/Q03/Jogador.c
@@ -25,14 +25,15 @@ typedef struct
 } Jogador;
 
 // Struct NO
-typedef struct No {
+typedef struct No
+{
     Jogador elemento;
     int nivel;
-    struct No* esq;
-    struct No* dir;
+    struct No *esq;
+    struct No *dir;
 } No;
 
-No* raiz = NULL;
+No *raiz = NULL;
 
 // Funcoes Jogador
 void initJogador(Jogador *j);
@@ -41,22 +42,26 @@ void tratamentoVirgula(char *linha, char *nova);
 void salvaPorId(Jogador *j, int id);
 void removeCaracteresEspeciais(char *str);
 void setJogador(Jogador *j, char *linha);
-char* imprimirJogador(Jogador j);
+char *imprimirJogador(Jogador j);
 void swap(Jogador *a, Jogador *b);
 // Funções da AVL
-No* criaNo(Jogador elemento);
+No *criaNo(Jogador elemento);
 int max(int a, int b);
-int altura(No* no);
-int fatorBalanceamento(No* no);
-No* rotacionarDir(No* no);
-No* rotacionarEsq(No* no);
+int altura(No *no);
+int fatorBalanceamento(No *no);
+No *rotacionarDir(No *no);
+No *rotacionarEsq(No *no);
 void inserindo(Jogador elemento);
-No* inserir(No* no, Jogador elemento);
-bool busca(No* no, char* str);
-void registroLog(long tempo, int comparacoes);
+No *inserir(No *no, Jogador elemento);
+bool buscaRec(No *no, char *str);
+bool busca(char *str);
+void caminharCentral();
+void caminharCentralRec(No *i);
+void registroLog();
 
-No* criaNo(Jogador elemento) {
-    No* novoNo = (No*)malloc(sizeof(No));
+No *criaNo(Jogador elemento)
+{
+    No *novoNo = (No *)malloc(sizeof(No));
     novoNo->elemento = elemento;
     novoNo->nivel = 1;
     novoNo->esq = NULL;
@@ -64,25 +69,29 @@ No* criaNo(Jogador elemento) {
     return novoNo;
 }
 
-int max(int a, int b) {
+int max(int a, int b)
+{
     return (a > b) ? a : b;
 }
 
-int altura(No* no) {
+int altura(No *no)
+{
     if (no == NULL)
         return 0;
     return no->nivel;
 }
 
-int fatorBalanceamento(No* no) {
+int fatorBalanceamento(No *no)
+{
     if (no == NULL)
         return 0;
     return altura(no->dir) - altura(no->esq);
 }
 
-No* rotacionarDir(No* no) {
-    No* noEsq = no->esq;
-    No* noEsqDir = noEsq->dir;
+No *rotacionarDir(No *no)
+{
+    No *noEsq = no->esq;
+    No *noEsqDir = noEsq->dir;
     noEsq->dir = no;
     no->esq = noEsqDir;
     no->nivel = max(altura(no->esq), altura(no->dir)) + 1;
@@ -90,9 +99,10 @@ No* rotacionarDir(No* no) {
     return noEsq;
 }
 
-No* rotacionarEsq(No* no) {
-    No* noDir = no->dir;
-    No* noDirEsq = noDir->esq;
+No *rotacionarEsq(No *no)
+{
+    No *noDir = no->dir;
+    No *noDirEsq = noDir->esq;
     noDir->esq = no;
     no->dir = noDirEsq;
     no->nivel = max(altura(no->esq), altura(no->dir)) + 1;
@@ -101,12 +111,15 @@ No* rotacionarEsq(No* no) {
     return noDir;
 }
 
-void inserindo(Jogador elemento){
+void inserindo(Jogador elemento)
+{
     raiz = inserir(raiz, elemento);
 }
 
-No* inserir(No* no, Jogador elemento) {
-    if (no == NULL){
+No *inserir(No *no, Jogador elemento)
+{
+    if (no == NULL)
+    {
         return criaNo(elemento);
     }
     if (strcmp(elemento.nome, no->elemento.nome) < 0)
@@ -126,12 +139,14 @@ No* inserir(No* no, Jogador elemento) {
     if (balanceamento < -1 && strcmp(elemento.nome, no->esq->elemento.nome) > 0)
         return rotacionarDir(no);
 
-    if (balanceamento > 1 && strcmp(elemento.nome, no->dir->elemento.nome) > 0) {
+    if (balanceamento > 1 && strcmp(elemento.nome, no->dir->elemento.nome) > 0)
+    {
         no->dir = rotacionarDir(no->dir);
         return rotacionarEsq(no);
     }
 
-    if (balanceamento < -1 && strcmp(elemento.nome, no->esq->elemento.nome) < 0) {
+    if (balanceamento < -1 && strcmp(elemento.nome, no->esq->elemento.nome) < 0)
+    {
         no->esq = rotacionarEsq(no->esq);
         return rotacionarDir(no);
     }
@@ -139,25 +154,54 @@ No* inserir(No* no, Jogador elemento) {
     return no;
 }
 
-bool busca(No* no, char* str) {
-    if (no == NULL) {
+// Função auxiliar para imprimir o caminho percorrido na pesquisa
+bool busca(char *nome)
+{
+    return buscaRec(raiz, nome);
+}
+
+// Função para realizar a pesquisa na árvore AVL
+bool buscaRec(No *no, char *nome)
+{
+    if (no == NULL)
+    {
+        printf(" NAO\n");
         return false;
     }
-    int comparacao = strcmp(str, no->elemento.nome);
-    if (comparacao < 0) {
-        printf(" esq"); 
-        return busca(no->esq, str);
-    } else if (comparacao > 0) {
-        printf(" dir"); 
-        return busca(no->dir, str);
-    } else {
+    int comparacao = strcmp(nome, no->elemento.nome);
+    if (comparacao < 0)
+    {
+        printf(" esq");
+        return buscaRec(no->esq, nome);
+    }
+    else if (comparacao > 0)
+    {
+        printf(" dir");
+        return buscaRec(no->dir, nome);
+    }
+    else
+    {
+        printf(" SIM\n");
         return true;
     }
 }
 
+void caminharCentral()
+{
+    printf("[ ");
+    caminharCentralRec(raiz);
+    printf("]");
+}
+void caminharCentralRec(No* i) {
+		if (i != NULL) {
+			caminharCentralRec(i->esq); // Elementos da esquerda.
+			printf("%s ",i->elemento.nome); // Conteudo do no.
+			caminharCentralRec(i->dir); // Elementos da direita.
+		}
+	}
 /*
-*   Funções de Jogador
-*/
+ *   Funções de Jogador
+ */
 // Inicializa o jogador
 void initJogador(Jogador *j)
 {
@@ -190,7 +234,8 @@ void addJogadores()
             inserindo(jogador);
         }
         scanf(" %[^\n]s", id);
-        if (strcmp(id, "FIM") == 0) isFim = true;
+        if (strcmp(id, "FIM") == 0)
+            isFim = true;
     }
     fflush(stdin);
 }
@@ -229,8 +274,9 @@ void tratamentoVirgula(char *linha, char *nova)
 }
 
 // Salva jogador por id, a partir do arquivo csv
-void salvaPorId(Jogador *j, int id) {
-    char path[] = "/tmp/players.csv";
+void salvaPorId(Jogador *j, int id)
+{
+    char path[] = "D:/Programacao/Cc-PucMG/2periodo/TrabalhosPraticos/TP2-Aeds2/players.csv";
     FILE *file = fopen(path, "r");
     if (file == NULL)
     {
@@ -329,10 +375,12 @@ void setJogador(Jogador *j, char *linha)
     }
 }
 
-void registroLog(long tempo, int comparacoes) {
+void registroLog()
+{
     FILE *file;
-    file = fopen("/tmp/806454_avl.txt", "w");
-    if (file == NULL) {
+    file = fopen("806454_avl.txt", "w");
+    if (file == NULL)
+    {
         perror("Erro ao abrir o arquivo.\n");
         return;
     }
@@ -342,19 +390,18 @@ void registroLog(long tempo, int comparacoes) {
 
 int main()
 {
-    printf("806454\tYago Almeida Melo\n\n");
     addJogadores();
+    caminharCentral();
     char str[100];
-    scanf(" %[^\n]s", str);
-    while (strcmp(str, "FIM") != 0) {
+    fgets(str, sizeof(str), stdin);
+    while (strcmp(str, "FIM") != 0)
+    {
         printf("%s raiz", str);
-        if(busca(raiz, str)) {
-            printf(" SIM\n");
-        } 
-        if(!busca(raiz, str)) {
-            printf(" NAO\n");
-        }
-        scanf(" %[^\n]s", str);
+        start = clock();
+        busca(str);
+        end = clock();
+        fgets(str, sizeof(str), stdin);
     }
-
+    tempo = ((double)end - start) / CLOCKS_PER_SEC;
+    registroLog();
 }
---
+++ 2periodo/TrabalhosPraticos/TP4-Aeds2/Q07/Jogador.java
@@ -0,0 +1,307 @@
+/*
+	806454 - Yago Almeida Melo
+	TP4 - Q07 / Hash com Reserva
+*/
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////// CLASSE Hash
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// ///////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Hash {
+    Jogador tabela[];
+    int m1, m2, m, reserva;
+    final int NULO = -1;
+    static int comparacoes = 0;
+    static double tempo = 0;
+
+    public Hash() {
+        this(21, 9);
+    }
+
+    public Hash(int m1, int m2) {
+        this.m1 = m1;
+        this.m2 = m2;
+        this.m = m1 + m2;
+        this.tabela = new Jogador[this.m];
+        for (int i = 0; i < m1; i++) {
+            tabela[i] = null;
+        }
+        reserva = 0;
+    }
+
+    public int h(Jogador elemento) {
+        return elemento.getAltura() % m1;
+    }
+
+    public void inserir(Jogador elemento) throws Exception {
+        int i = h(elemento);
+        if (elemento.getId() == -1) {
+            throw new Exception("Erro!");
+        } else if (tabela[i] == null) {
+            tabela[i] = elemento;
+        } else if (reserva < m2) {
+            tabela[m1 + reserva] = elemento;
+            reserva++;
+        } else {
+            throw new Exception("Erro!");
+        }
+    }
+
+    public void pesquisar() {
+        String elemento = MyIO.readLine();
+        while (!elemento.equals("FIM")) {
+            boolean resp = false;
+            MyIO.print(elemento);
+            double start = System.nanoTime();
+            for (int i = 0; i < m && resp == false; i++) {
+                if (tabela[i] != null && tabela[i].getNome().equals(elemento)) {
+                    MyIO.print(" SIM\n");
+                    resp = true;
+                }
+            }
+            double end = System.nanoTime();
+            tempo += end - start;
+            if(resp == false){
+                MyIO.print(" NAO\n");
+            }
+            elemento = MyIO.readLine();
+        }   
+    }
+
+    /*
+     * Metodo para registrar o log de execucao com movimentacoes
+     */
+    public static void registroLog() {
+        File file = new File("/tmp/806454_arvoreArvore.txt");
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file, false);
+            fw.write("Matricula: 806454\t" + "Comparacoes: " + comparacoes + '\t' + "Tempo de execucao: " + tempo
+                    + " milisegundos");
+            fw.close();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////// CLASSE JOGADOR
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// ///////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Jogador {
+    /*
+     * Atributos da classe Jogador
+     */
+    static int tamanho;
+    private int id, altura, peso, anoNascimento;
+    private String nome, universidade, cidadeNascimento, estadoNascimento;
+
+    /*
+     * Construtores da classe Jogador
+     */
+    Jogador() {
+        this.id = -1;
+        this.altura = -1;
+        this.peso = -1;
+        this.anoNascimento = -1;
+        this.nome = null;
+        this.universidade = null;
+        this.cidadeNascimento = null;
+        this.estadoNascimento = null;
+    }
+
+    Jogador(int id, int altura, int peso, int anoNascimento, String nome, String universidade,
+            String cidadeNascimento,
+            String estadoNascimento) {
+        this.id = id;
+        this.altura = altura;
+        this.peso = peso;
+        this.anoNascimento = anoNascimento;
+        this.nome = nome;
+        this.universidade = universidade;
+        this.cidadeNascimento = cidadeNascimento;
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    /*
+     * Gets e Sets da classe Jogador
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setUniversidade(String universidade) {
+        this.universidade = universidade;
+    }
+
+    public String getUniversidade() {
+        return universidade;
+    }
+
+    public void setCidadeNascimento(String cidadeNascimento) {
+        this.cidadeNascimento = cidadeNascimento;
+    }
+
+    public String getCidadeNascimento() {
+        return cidadeNascimento;
+    }
+
+    public void setEstadoNascimento(String estadoNascimento) {
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    public String getEstadoNascimento() {
+        return estadoNascimento;
+    }
+
+    /*
+     * Metodo para setar os dados do arquivo csv
+     */
+    public void setDados(String linha) {
+        String[] dados = linha.split(",");
+        setId(Integer.parseInt(dados[0]));
+        setNome(dados[1]);
+        setAltura(Integer.parseInt(dados[2]));
+        setPeso(Integer.parseInt(dados[3]));
+        if (dados.length > 4 && !dados[4].isEmpty())
+            setUniversidade(dados[4]);
+        else
+            setUniversidade("nao informado");
+        setAnoNascimento(Integer.parseInt(dados[5]));
+        if (dados.length > 6 && !dados[6].isEmpty())
+            setCidadeNascimento(dados[6]);
+        else
+            setCidadeNascimento("nao informado");
+        if (dados.length > 7 && !dados[7].isEmpty())
+            setEstadoNascimento(dados[7]);
+        else
+            setEstadoNascimento("nao informado");
+    }
+
+    /*
+     * Metodo para clonar um objeto da classe Jogador
+     */
+    public Jogador clone() throws CloneNotSupportedException {
+        Jogador novo = new Jogador();
+        novo.id = this.id;
+        novo.altura = this.altura;
+        novo.peso = this.peso;
+        novo.anoNascimento = this.anoNascimento;
+        novo.nome = this.nome;
+        novo.universidade = this.universidade;
+        novo.cidadeNascimento = this.cidadeNascimento;
+        novo.estadoNascimento = this.estadoNascimento;
+        return novo;
+    }
+
+    /*
+     * Metodo para imprimir os dados da classe Jogador
+     */
+    public void Imprimir(int id) {
+        System.out.println(toString());
+    }
+
+    /*
+     * Metodo para transformar em string os dados da classe Jogador
+     */
+    public String toString() {
+        String resp = getId() + " ## " + getNome() + " ## " + getAltura() + " ## " + getPeso() + " ## "
+                + getAnoNascimento() + " ## " + getUniversidade() + " ## " + getCidadeNascimento() + " ## "
+                + getEstadoNascimento() + " ##";
+        return resp;
+    }
+
+    /*
+     * Metodo para add jogadores em um array
+     */
+    public static void addJogadores(Hash hash) {
+        String id = MyIO.readLine();
+        boolean fim = false;
+        while (!fim) {
+            try {
+                Jogador jogador = new Jogador();
+                jogador.procuraID(id);
+                hash.inserir(jogador);
+            } catch (Exception e) {
+                e.getMessage();
+            }
+            id = MyIO.readLine();
+            if (id.equals("FIM"))
+                fim = true;
+        }
+    }
+
+    /*
+     * Metodo para procurar um jogador pelo id e salva
+     */
+    public void procuraID(String id) throws Exception {
+        FileReader file = new FileReader("/tmp/players.csv");
+        BufferedReader bf = new BufferedReader(file);
+        Integer idInt = Integer.parseInt(id);
+        String linha;
+        while ((linha = bf.readLine()) != null) {
+            String numId = linha.substring(0, linha.indexOf(","));
+            try {
+                if (Integer.parseInt(numId) == idInt) {
+                    setDados(linha);
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                e.getMessage();
+            }
+        }
+        bf.close();
+        file.close();
+    }
+
+    public static void main(String[] args) {
+        Hash hash = new Hash();
+        addJogadores(hash);
+        hash.pesquisar();
+    }
+}
