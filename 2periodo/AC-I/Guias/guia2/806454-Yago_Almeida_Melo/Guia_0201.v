6a4fc64f18477494af6456c0fe51181178017a10
Yago Almeida Melo <yagoalmeidamelo27102004@gmail.com.com>
2024-02-06 12:31:59 -0300

    Ac: guia2/ in progress

--- 2periodo/AC-I/Guias/guia2/806454-Yago_Almeida_Melo/Guia_0201.v
+++ 2periodo/AC-I/Guias/guia2/806454-Yago_Almeida_Melo/Guia_0201.v
@@ -4,90 +4,72 @@
 */
 module Guia_0201;
 // define data
-	reg [7:0] a = 8'b01101000; // binary (only fraction part, Big Endian)
-	reg [7:0] b = 8'b10011000; // binary (only fraction part, Big Endian)
-	reg [7:0] c = 8'b10101000; // binary (only fraction part, Big Endian)
-	reg [7:0] d = 8'b11001000; // binary (only fraction part, Big Endian)
-	reg [7:0] e = 8'b11011000; // binary (only fraction part, Big Endian)
-	real x = 0 ; // decimal
-	real power2 = 1.0; // power of 2
-	integer y = 7 ; // counter
-// actions
+	reg [7:0] a = 8'b00101000; 
+	reg [7:0] b = 8'b01011000; 
+	reg [7:0] c = 8'b10001000; 
+	reg [7:0] d = 8'b11011000; 
+	reg [7:0] e = 8'b10011000; 
+	real x = 0 ; 
+	real power2 = 1.0; 
+	integer y = 7 ; 
+
 initial
 begin : main
 	$display ( "Guia_0201 - Binario com parte fracionaria para decimal\n" );
-	
-	//1b.)a)
 	while ( y >= 0 ) begin
 		power2 = power2 / 2.0;
 		if ( a[y] == 1 ) begin
 			x = x + power2;
 		end
 		y=y-1;
-	end // end while
+	end 
 	$display ("a) 0.%b(2) = %f(10)", a, x );
-
-	// Reset
 	x = 0;
 	power2 = 1.0;
 	y = 7;
-
-	//1b.)b)
 	while ( y >= 0 ) begin
 		power2 = power2 / 2.0;
 		if ( b[y] == 1 ) begin 
 			x = x + power2;
 		end
 		y=y-1;
-	end // end while
+	end 
 	$display ("b) 0.%b(2) = %f(10)", b, x );
-
-	// Reset
 	x = 0;
 	power2 = 1.0;
 	y = 7;
-
-	//1b.)c)
 	while ( y >= 0 ) begin
 		power2 = power2 / 2.0;
 		if ( c[y] == 1 ) begin
 			x = x + power2;
 		end
 		y=y-1;
-	end // end while
+	end 
 	$display ("c) 0.%b(2) = %f(10)", c, x );
-
-	// Reset
 	x = 0;
 	power2 = 1.0;
 	y = 7;
-
-	//1b.)d)
 	while ( y >= 0 ) begin
 		power2 = power2 / 2.0;
 		if ( d[y] == 1 ) begin
 			x = x + power2;
 		end
 		y=y-1;
-	end // end while
-	x=x+1;// parte inteira
+	end 
+	x=x+1;
 	$display ("d) 1.%b(2) = %f(10)", d, x );
-
-	// Reset
 	x = 0;
 	power2 = 1.0;
 	y = 7;
-
-	//1b.)e)
 	while ( y >= 0 ) begin
 		power2 = power2 / 2.0;
 		if ( e[y] == 1 ) begin
 			x = x + power2;
 		end
 		y=y-1;
-	end // end while
-	x=x+3; // parte inteira
+	end 
+	x=x+3; 
 	$display ("e) 11.%b(2) = %f(10)", e, x );
 
-end // main
-endmodule // Guia_0201
+end 
+endmodule 
--- 2periodo/AC-I/Guias/guia2/806454-Yago_Almeida_Melo/Guia_0204.v
+++ 2periodo/AC-I/Guias/guia2/806454-Yago_Almeida_Melo/Guia_0204.v
@@ -5,19 +5,20 @@
 
 module Guia_0204;
 // data
-reg [15:0] a = 8'o312; 
-reg [15:0] b = 16'h3e7;
-reg [15:0] c = 16'o751;
-reg [15:0] d = 16'o231;
-reg [15:0] e = 16'hb52;
+reg [15:0] a = 8'o213; 
+reg [15:0] b = 16'h4a2;
+reg [15:0] c = 16'o747;
+reg [15:0] d = 16'o654;
+reg [15:0] e = 16'ha51;
+reg [15:0] bin;
 // actions
 initial
 begin : main
 $display ( "Guia_0204 - Bases 4,8,16 fracionarias paea bases 2 e 4" );
-$display ( "a = 0.%o (4) = 0.%b%b%b (2)", a[8:0], a[8:6],a[5:3],a[2:0] );
-$display ( "b = 0.%h (16) = 0.%o%o%o (4)", b[11:0], b[11:8],b[7:6],b[1:0] );
-$display ( "c = 0.%o (8) = 0.%b%b%b (2)", c[8:0], c[12:7],c[7:4],c[3:0] );
-$display ( "d = 7.%o (8) = 13.%o%o%o (4)", d[8:0], d[11:8],d[7:4],d[3:0] );
-$display ( "e = A.%h (16) = 22.%o%o%o (4)", e[11:0], e[12:10],e[8:5],e[4:3] );
+$display ( "a = 0.%o (4) = 0.%b%b%b (2)", a[7:0],a[7:6],a[4:3],a[1:0] );
+$display ( "b = 0.%h (16) = 0.%o%o%o%o%o%o (4)", b[11:0], b[11:10],b[9:8],b[7:6],b[5:4],b[3:2],b[1:0] );
+$display ( "c = 0.%o (8) = 0.%b%b%b (2)", c[8:0], c[8:5],c[4:2],c[1:0] );
+$display ( "d = 7.%o (8) = 13.%o%o%o%o (4)", d[8:0], d[8:7],d[5],d[3],d[2:1] );
+$display ( "e = f.%h (16) = 33.%o%o%o%o%o%o (4)", e[11:0], e[11:10],e[9:8],e[7:6],e[5:4],e[3:2],e[1:0] );
 end // main
 endmodule // Guia_0204
