2a46822427d19c2e61efb3e15f703c71fc87de62
yago-almeida-melo <yagoalmeidamelo27102004@gmail.com>
2024-03-03 00:43:11 -0300

    TP1- aleatoria em C 79% e Arquivo java 100%

--- 2periodo/TrabalhosPraticos/2024-1/TP1-Aeds2/AlteracaoAleatoria/aleatoria.c
+++ 2periodo/TrabalhosPraticos/2024-1/TP1-Aeds2/AlteracaoAleatoria/aleatoria.c
@@ -7,43 +7,51 @@ bool isFim(char ptr[]);
 char * alteracaoAleatoria(char ptr[]);
 char * alteracao(char ptr[], int i);
 
-
+/*
+*   function: isFim
+*   @params: char[]
+*   action: Retorna verdadeiro se string é igual a "FIM" e falso se não for.
+*/
 bool isFim(char ptr[]){
-    return strlen(ptr) == 3 && ptr[0] == 'F' && ptr[1] == 'I' && ptr[2] == 'M';
+    return strlen(ptr) == 4 && ptr[0] == 'F' && ptr[1] == 'I' && ptr[2] == 'M' && ptr[3] == '\n';
 }
 
+/*
+*   function: alteracaoAleatoria
+*   @params: char[]
+*   action: chamada para funcao recursiva alteracao e retorna a string modificada
+*/
 char * alteracaoAleatoria(char ptr[]){
     return alteracao(ptr, 0);
+     
 }
 
+/*
+
+*   function: alteracao
+*   @params: char[] & int
+*   action: funcao recursiva que faz a alteracao e retorna a string modificada
+*/
 char * alteracao(char ptr[], int i){
-    char *resp = malloc((strlen(ptr)+1)*sizeof(char));
     char atual = ('a' + rand() % 26);
     char nova = ('a' + rand() % 26);
-    if(i < strlen(ptr)-1){
+    if(i < strlen(ptr)){
         if(ptr[i] == atual){
-            resp[i] = nova;
-        } else{
-            resp[i] = ptr[i];
+            ptr[i] = nova;
         }
-        char *recursiveResult = alteracao(ptr, i + 1);
-        strcpy(resp + i + 1, recursiveResult); // Concatenate the results
-        free(recursiveResult);
-    } else {
-        resp[i] = '\0'; // Terminate the string
+        alteracao(ptr, i+1);
     }
-    return resp;
+    return ptr;
 }    
 
 int main(){
     char input[1000];
     srand(4);
-    scanf("%[^\n]s", input); 
+    fgets(input, sizeof(input), stdin);
     while(!isFim(input)){
         char *resp = alteracaoAleatoria(input);
-        printf("%s\n", resp);
-        scanf("%[^\n]s", input);
+        printf("%s", resp);
+        fgets(input, sizeof(input), stdin);
     }
-    free(input);
     return 0;
 }
