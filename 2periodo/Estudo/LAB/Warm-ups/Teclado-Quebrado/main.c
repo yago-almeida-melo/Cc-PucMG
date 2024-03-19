3b4212123fca50022b940a3e0990fb65673f6746
yago-almeida-melo <yagoalmeidamelo27102004@gmail.com>
2024-03-18 21:37:39 -0300

    organizando...

--- 2periodo/Estudo/LAB/Warm-ups/Teclado-Quebrado/main.c
+++ 2periodo/Estudo/LAB/Warm-ups/Teclado-Quebrado/main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAX_LETTERS 100001
+#define MAX_LETTERS 100000
 
 /*
 *   806454 - Yago Almeida Melo
@@ -12,32 +12,30 @@
 
 char* substring(char x[], int i){
     int j = 0;
-    char resp[MAX_LETTERS];
+    char* resp = (char*)malloc(MAX_LETTERS * sizeof(char)); 
     if (resp == NULL) {
         return NULL;
     }
-    while(x[i]!='\0' && (x[i]!=']' || x[i]!='[')){ 
+    while(x[i]!='\0' && x[i]!=']'){ 
         resp[j++] = x[i++];
     }
-    resp[j] = '\0';
-    printf("\nSubstring: %s\n", resp); 
-    return resp;
+    resp[j] = '\0'; 
 }
 
 
 char* teclado(char in[]){
-    char resp[MAX_LETTERS];
-    char sub[] = "";
+    char* resp = (char*)malloc(MAX_LETTERS * sizeof(char)); 
+    char* sub = (char*)malloc(MAX_LETTERS * sizeof(char));
     if (resp == NULL) {
         return NULL;
     }
+    resp[0] = '\0';
     for(int i = 0; i < strlen(in); i++){
         if(in[i] != '['){
             strncat(resp, &in[i], 1); 
         } else {
-            strcat(sub ,substring(in, i + 1));
+            sub = substring(in, i + 1);
             if (sub != NULL) {
-                printf("Inserino: %s", sub);
                 strcat(sub, resp); 
             }
             while(in[i] != ']' && in[i] != '\0') {
@@ -51,9 +49,11 @@ char* teclado(char in[]){
 
 
 int main(){
-    char input[MAX_LETTERS];
-    while(strcmp(scanf("%s", input), "FIM") != 0){
+    /*char input[MAX_LETTERS];
+    while(scanf("%s", input) != EOF){
         printf("%s\n", teclado(input));
-    }
+    }*/
+    char in[MAX_LETTERS] = "opa[e]io[u]";
+    printf("\n%s\n", teclado(in));
     return 0;
 }
