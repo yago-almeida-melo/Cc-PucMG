bef4abcc061e65ec3628556f0028a77f175fb75e
yago-almeida-melo <yagoalmeidamelo27102004@gmail.com>
2024-04-09 21:49:29 -0300

    AC1: guia9 in progress...

--- 2periodo/AC-I/Guias/guia9/Guia_0901.v
+++ 2periodo/AC-I/Guias/guia9/Guia_0901.v
@@ -1,85 +1,52 @@
-/*
-	Guia_0901.v
-	806454 - Yago Almeida Melo
-*/
-
-
-module PulseGen(
-    input wire clk,
-    output reg pulse
-);
-    always @(posedge clk) begin
-        pulse <= 1'b1; 
-        #3 pulse <= 1'b0;  
-    end
-endmodule
-module TriggerGen(
-    input wire clk,
-    input wire trigger,
-    output reg signal
-);
-    always @(posedge clk) begin
-        if (trigger) begin
-            signal <= 1'b1;  
-            #60 signal <= 1'b0;  
-        end else begin
-            signal <= 1'b0;
-        end
-    end
-endmodule
-module top(
-    input wire clk,
-    input wire trigger,
-    output wire pulse,
-    output wire signal
-);
-    PulseGen PulseGenInstance(
-        .clk(clk),
-        .pulse(pulse)
-    );
-
-    TriggerGen TriggerGenInstance(
-        .clk(clk),
-        .trigger(trigger),
-        .signal(signal)
-    );
-endmodule
-module clock(
-    output reg clk
-);
+// ---------------------------
+// -- test clock generator (2)
+// ---------------------------
+module clock ( output clk );
+    reg clk;
     initial begin
         clk = 1'b0;
-        forever #10 clk = ~clk;  // Gera um clock com período de 20 unidades de tempo
+    end
+    always begin
+        #12 clk = ~clk;
     end
 endmodule
+
+module pulse ( signal, clock );
+    input clock;
+    output signal;      
+    reg signal;
+    always @ ( clock ) begin
+        signal = 1'b1;
+        #3 signal = 1'b0;
+        #3 signal = 1'b1;
+        #3 signal = 1'b0;
+    end
+endmodule // pulse
+
+module trigger ( signal, on, clock );
+    input on, clock;
+    output signal;
+    reg signal;
+    always @ ( posedge clock & on ) begin
+        #60 signal = 1'b1;
+        #60 signal = 1'b0;
+    end
+endmodule // trigger
+
 module Guia_0901;
     wire clock;
+    clock clk ( clock );
     reg p;
     wire p1,t1;
-
-    
-    clock clk(clock);
-
-    
-    PulseGen pulse1(
-        .clk(clock),
-        .pulse(p1)
-    );
-
-    
-    TriggerGen trigger1(
-        .clk(clock),
-        .trigger(p),
-        .signal(t1)
-    );
-
+    pulse pulse1 ( p1, clock );
+    trigger trigger1 ( t1, p, clock );
     initial begin
         p = 1'b0;
     end
-
     initial begin
         $dumpfile ( "Guia_0901.vcd" );
         $dumpvars ( 1, clock, p1, p, t1 );
+        $monitor ("clock = %b / pulse = %b / trigger = %b", clock, p1, t1);
         #060 p = 1'b1;
         #120 p = 1'b0;
         #180 p = 1'b1;
