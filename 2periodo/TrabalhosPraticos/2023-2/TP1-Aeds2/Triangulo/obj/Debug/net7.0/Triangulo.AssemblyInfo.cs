da4929cd24c3ecbec87c31048ef1a7e4fdcd50cc
yago-almeida-melo <yagoalmeidamelo27102004@gmail.com>
2024-05-21 14:44:54 -0300

    .

--- 2periodo/TrabalhosPraticos/2023-2/TP1-Aeds2/Triangulo/obj/Debug/net7.0/Triangulo.AssemblyInfo.cs
+++ 2periodo/TrabalhosPraticos/2023-2/TP1-Aeds2/Triangulo/obj/Debug/net7.0/Triangulo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Triangulo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc5f2ad948e5da23317430cd9026870e2dd5093")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Triangulo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Triangulo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
