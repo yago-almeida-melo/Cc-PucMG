5986e710693b7efd54310811f2d9ecbd191adb3f
yago-almeida-melo <yagoalmeidamelo27102004@gmail.com>
2024-05-23 18:48:31 -0300

    .

--- 2periodo/TrabalhosPraticos/2024-1/TP1-Aeds2/Palindromo/obj/Debug/net7.0/Palindromo.AssemblyInfo.cs
+++ 2periodo/TrabalhosPraticos/2024-1/TP1-Aeds2/Palindromo/obj/Debug/net7.0/Palindromo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Palindromo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e57f65c19cbb1eb21c027d95ac265c1f3157b8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Palindromo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Palindromo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
