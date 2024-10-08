d0fbb6b65bbc46d548099f0a0275f037fb243aa3
yago-almeida-melo <yagoalmeidamelo27102004@gmail.com>
2024-10-08 19:06:39 -0300

    TP2: copying files

--- 3periodo/Aeds-III/TP1/Registro.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public interface Registro {
-    public void setId(int i);
-    public int getId();
-    public byte[] toByteArray() throws IOException;
-    public void fromByteArray(byte[] b) throws IOException;   
-}
