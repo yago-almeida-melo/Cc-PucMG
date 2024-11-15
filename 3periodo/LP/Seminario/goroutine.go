a57beb97fdbdafc5f4c169cd3c4cb642e66e6cca
yago-almeida-melo <yagoalmeidamelo27102004@gmail.com>
2024-11-14 21:02:03 -0300

    .

--- 3periodo/LP/Seminario/goroutine.go
+++ 3periodo/LP/Seminario/goroutine.go
@@ -12,20 +12,33 @@ import (
 	a palavra chave go à frente da função invocada.
 */
 
-func function(id int, t time.Duration) {
+// Função que simula uma tarefa, recebendo um id e uma duração
+func function(id int, t time.Duration, done chan bool) {
 	for i := 0; i < 10; i++ {
 		time.Sleep(t)
 		fmt.Printf("Function %02d: %d\n", id, i)
 	}
+	// Envia um sinal para o canal indicando que terminou
+	done <- true
 }
 
 func main() {
 	fmt.Println("Starting...")
 
-	go function(1, time.Second*1)
-	go function(2, time.Second*1)
-	go function(3, time.Second*1)
+	//Canal de sincronização
+	done := make(chan bool, 3)
 
-	time.Sleep(time.Second * 5)
+	/* Inicializa as goroutines e
+	passa o canal como argumento*/
+	go function(1, time.Second*1, done)
+	go function(2, time.Second*1, done)
+	go function(3, time.Second*1, done)
+
+	/* Espera o sinal de cada goroutine
+	para saber que todas terminaram*/
+	for i := 0; i < 3; i++ {
+		<-done
+	}
+
 	fmt.Println("Ending...")
 }
