9c77cdd9e3499f7cd95ef74f41892ff29c83e6f6
yago-almeida-melo <yagoalmeidamelo27102004@gmail.com>
2026-02-21 15:09:31 -0300

    Comp. paralela tarefa1

--- 6periodo/Computação Paralela/Tarefa01/tarefa1.c
+++ 6periodo/Computação Paralela/Tarefa01/tarefa1.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <omp.h>
 
-int main()
-{
+int main() {
     #pragma omp parallel num_threads(2)   // seta o número de threads em 2
-    {    
-        int tid = omp_get_thread_num(); // lê o identificador da thread
-        
-        #pragma omp for ordered schedule(static) // diretiva para paralelizar o loop com ordenação
-        for (int i = 1; i <= 3; i++)
-        {
-            #pragma omp ordered // diretiva para garantir a ordem de execução       
-            {
+    {   
+        int tid = omp_get_thread_num();  // lê o identificador da thread
+        #pragma omp for ordered         // paraleliza o loop com ordenação
+        for (int i = 1; i <= 3; i++) {
+            #pragma omp ordered 
+            {       // garante a ordem de execução       
                 printf("[PRINT1] T%d = %d \n", tid, i);
                 printf("[PRINT2] T%d = %d \n", tid, i);
             }
