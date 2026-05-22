b84f48219dc0fd45adf055865e362b6d0f733552
Yago Almeida Melo <yagoalmeidamelo27102004@gmail.com>
2026-05-22 16:33:36 -0300

    Tarefa10: finished but I we see more later

---
+++ 6periodo/Computação Paralela/Tarefa10/crivo.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <omp.h>
+
+int sieveOfEratosthenes(int n)
+{
+   // Create a boolean array "prime[0..n]" and initialize
+   // all entries it as true. A value in prime[i] will
+   // finally be false if i is Not a prime, else true.
+   int primes = 0;
+   bool *prime = (bool *)malloc((n + 1) * sizeof(bool));
+   int sqrt_n = sqrt(n);
+
+   memset(prime, true, (n + 1) * sizeof(bool));
+
+   #pragma omp parallel
+{
+    for (int p = 2; p <= sqrt_n; p++) {
+        if (prime[p]) {
+            #pragma omp for schedule(static)
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+        #pragma omp barrier
+    }
+}
+   // count prime numbers
+   #pragma omp parallel for reduction(+ : primes) // Paraleliza o loop de contagem de primos
+   for (int p = 2; p <= n; p++)                // reduction para garantir o resultado correto
+      if (prime[p])
+         primes++;
+
+   return (primes);
+}
+
+int main() {
+   int n = 100000000;
+   omp_set_num_threads(2);  // limita a 2 threads em todo o programa
+   // se nao limitar, o OpenMP usará o número de threads igual ao número de núcleos disponíveis
+   printf("%d\n", sieveOfEratosthenes(n));
+   return 0;
+}
